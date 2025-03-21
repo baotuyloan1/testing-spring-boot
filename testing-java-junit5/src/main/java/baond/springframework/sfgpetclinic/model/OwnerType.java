cb73433d3b8aabd2b0d3fcae318cf200f668fece
baotuyloan <nguyenducbaokey@gmail.com>
2025-03-21 17:27:06 +0700

    - EnumSource annotation - ValueSource annotation

---
+++ testing-java-junit5/src/main/java/baond/springframework/sfgpetclinic/model/OwnerType.java
@@ -0,0 +1,9 @@
+package baond.springframework.sfgpetclinic.model;
+
+/**
+ * Created by BaoND on 2025-03-21
+ */
+public enum OwnerType {
+
+    INDIVIDUAL, COMPANY
+}
--- testing-java-junit5/src/test/java/baond/springframework/sfgpetclinic/model/OwnerTest.java
+++ testing-java-junit5/src/test/java/baond/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,11 @@
 package baond.springframework.sfgpetclinic.model;
 
 import baond.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,10 +24,24 @@ class OwnerTest implements ModelTests {
         assertAll("Properties test",
                 () -> assertAll("Person properties",
                         () -> assertEquals("Bao1", owner.getFirstName(), "First Name Did not Match"),
-                        ()-> assertEquals("Nguyen", owner.getLastName())),
+                        () -> assertEquals("Nguyen", owner.getLastName())),
                 () -> assertAll("Owner properties",
                         () -> assertEquals("1234-5678", owner.getTelephone()),
                         () -> assertEquals("123 Main Street 1", owner.getAddress(), "Address Did not Match"))
-                );
+        );
+    }
+
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - {index} : {arguments}")
+    @ValueSource( strings = {"Spring", "Framework", "BaoND"})
+    void testValueSource(String val) {
+        System.out.println(val);
+    }
+
+    @DisplayName("Enum Source Test")
+    @ParameterizedTest(name = "{displayName} - {index} : {arguments}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
     }
 }
