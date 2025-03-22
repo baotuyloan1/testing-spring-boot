025010c00e419dbad96ee666bd701ae541e15bbc
baotuyloan <nguyenducbaokey@gmail.com>
2025-03-22 14:31:09 +0700

    JUnit Extensions

---
+++ testing-java-junit5/src/test/java/baond/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -0,0 +1,34 @@
+package baond.springframework.sfgpetclinic.services.springdatajpa;
+
+import baond.springframework.sfgpetclinic.junitextensions.TimingExtention;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by BaoND on 2025-03-21
+ */
+@ExtendWith(TimingExtention.class)
+class PetSDJpaServiceIT {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
